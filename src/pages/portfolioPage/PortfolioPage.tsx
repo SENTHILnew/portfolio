c4988f5a53b334bd412864b329bf6fa2cbc14665
Senthilnathan Mani <31062212+SENTHILnew@users.noreply.github.com>
2023-10-09 23:56:47 +0530

    updated source code link

--- src/pages/portfolioPage/PortfolioPage.tsx
+++ src/pages/portfolioPage/PortfolioPage.tsx
@@ -3,10 +3,17 @@ import Header from '../../components/header/header'
 import classes from './PortfolioPage.module.scss'
 import Footer from '../../components/footer/footer'
 import Content from '../../components/content/content'
+import openout from '../../assets/images/maximize-size-option.svg'
 
 const PortfolioPage: React.FC = () => {
     return (
         <div className={classes['page-container']}>
+            <img
+                src={openout}
+                className="w-6 h-6 absolute top-4 right-4 bg-white openCode cursor-pointer"
+                title="Open source code"
+                onClick={() => window.open('https://github.com/SENTHILnew/portfolio', '_blank')}
+            ></img>
             <Header />
             <div className={classes['content-container']}>
                 <Content />
